6e178470def8d345044c482f4a2a91600f9d9972
Fernando Ariel Figliola <fernando.figliola@qualis-lab.com>
2021-06-02 19:13:07 -0300

    Se agregan 3s de espera en el script SelectProduct

--- GitTests/Actions/SelectProduct.cs
+++ GitTests/Actions/SelectProduct.cs
@@ -85,11 +85,14 @@ namespace GitTests.Actions
             Report.Log(ReportLevel.Info, "Wait", "Waiting 30s to exist. Associated repository item: 'ApplicationUnderTest.Content.lnk_MacBook'", repo.ApplicationUnderTest.Content.lnk_MacBookInfo, new ActionTimeout(30000), new RecordItemIndex(1));
             repo.ApplicationUnderTest.Content.lnk_MacBookInfo.WaitForExists(30000);
             
-            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ApplicationUnderTest.Content.lnk_MacBook' at 22;12.", repo.ApplicationUnderTest.Content.lnk_MacBookInfo, new RecordItemIndex(2));
+            Report.Log(ReportLevel.Info, "Delay", "Waiting for 3s.", new RecordItemIndex(2));
+            Delay.Duration(3000, false);
+            
+            Report.Log(ReportLevel.Info, "Mouse", "Mouse Left Click item 'ApplicationUnderTest.Content.lnk_MacBook' at 22;12.", repo.ApplicationUnderTest.Content.lnk_MacBookInfo, new RecordItemIndex(3));
             repo.ApplicationUnderTest.Content.lnk_MacBook.Click("22;12");
             Delay.Milliseconds(200);
             
-            Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (InnerText='$602.00') on item 'ApplicationUnderTest.Content.lbl_Precio'.", repo.ApplicationUnderTest.Content.lbl_PrecioInfo, new RecordItemIndex(3));
+            Report.Log(ReportLevel.Info, "Validation", "Validating AttributeEqual (InnerText='$602.00') on item 'ApplicationUnderTest.Content.lbl_Precio'.", repo.ApplicationUnderTest.Content.lbl_PrecioInfo, new RecordItemIndex(4));
             Validate.AttributeEqual(repo.ApplicationUnderTest.Content.lbl_PrecioInfo, "InnerText", "$602.00");
             Delay.Milliseconds(100);
             
